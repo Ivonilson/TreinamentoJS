a6c890b6918f727bcc851634460a4166e1e8d18f
Ivonilson <ivonilsoncardoso@hotmail.com>
2019-09-08 22:36:46 -0300

    basico: declaração de variáveis, constantes e tipagem

---
+++ exercicios-js/variaveis/tipagemDinamica/tipagem.js
@@ -0,0 +1,9 @@
+//JS é uma linguagem fracamente tipada ou com tipagem dinâmica
+let qualquer = 'Legal'
+console.log(qualquer)
+
+//verificando o tipo de valor atribuído a variavel
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(typeof qualquer)
---
+++ exercicios-js/variaveis/variaveisEconstantes.js
@@ -0,0 +1,23 @@
+//atribuição de valor a uma variável
+
+//a variável declarada com var pode ser redeclarada novamente, o JS não coloca restrição sobre isso.
+var a = 3
+//uma vez declarada uma variável com let, a mesma não pode ser redeclarada. 
+let b = 4
+
+var a = 30
+
+//console.log(a, b)
+
+a = 300
+b = 400
+
+//console.log(a, b)
+
+//declaração de uma constante no JS
+const c = 5
+
+//obviamente uma constante não pode ter seu valor reatribuído fora da declaração da mesma. A ação abaixo causa um erro.
+//c = 50
+console.log(c)
+
