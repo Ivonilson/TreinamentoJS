e5db78dab5ca266b2e41df2732294c95a593f9f1
Ivonilson <ivonilsoncardoso@hotmail.com>
2019-11-12 11:34:23 -0200

    strings e funcoes Math

---
+++ exercicios-js/fundamentos/strings.js
@@ -0,0 +1,8 @@
+const escola = "Cod3er"
+
+console.log(escola.charAt(4)) //retorna da string conforme especificação do índice.
+console.log(escola.charAt(8)) //como extrapolou o último índice existente, vai retornar vazio. Não dará erro.
+
+console.log(escola.charCodeAt(3)) //retorna o valor do caractere referente ao índice da string com sua representação na tabela UNICODE
+console.log(escola.indexOf('r')) //retorna o primeiro índice ao qual o caractere está posicionado em relação a string alvo.
+
