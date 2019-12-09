0a9fb8a8474edd99eea191c30ff323f5009a8c39
Ivonilson <ivonilsoncardoso@hotmail.com>
2019-12-09 11:29:10 -0300

    arrays e boolean

---
+++ exercicios-js/fundamentos/arrays.js
@@ -0,0 +1,25 @@
+//o ARRAY em JS é heterogêneo, ou seja, pode receber vários tipos de dados em no mesmo array.
+//não tem tamanho fixo (como no JAVA por exemplo), ou seja, o mesmo cresce dinamicamente
+const valores = [7.7, 5.2, 7.8, 9.1]
+//console.log(valores[0], valores[3])
+//console.log(valores[4])
+
+//atribuindo valor a uma elemento do array
+//valores[4] = 10
+//console.log(valores[4])
+
+//incluindo elementos em um array
+//valores.push({id: 4}, false, null, 'teste')
+//valores.push(4)
+
+//exibindo a quantidade de itens de um array
+
+//retira o último valor do array e retorno o valor do mesmo
+//console.log(valores.pop())
+
+//deletendo elementos do array
+delete valores[0]
+
+console.log(valores[1])
+
+
