65136245c3cfa7b6fa8eabbfbe0569a9ed91d159
unknown <ivonilsoncardoso@hotmail.com>
2020-09-16 00:52:15 -0300

    update

--- exercicios-js/fundamentos/funcao/arrowFunction3.js
+++ exercicios-js/fundamentos/funcao/arrowFunction3.js
@@ -2,3 +2,15 @@ let ComparaComThis = function(param){
     console.log(this === param)
 }
 
+ComparaComThis(global);
+
+const obj = {}
+ComparaComThis = ComparaComThis.bind(obj)
+ComparaComThis(global)
+ComparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+
+comparaComThisArrow(module.exports)
+comparaComThisArrow(this)
